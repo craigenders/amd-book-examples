5498c89b08bf90aa48b1bac77c32c45e3aa72d3e
craigenders <cenders@ucla.edu>
2025-07-27 15:57:31 -0700

    Update

---
+++ rblimp/8.3 - Bayes MLM w Multivariate Predictors.R
@@ -0,0 +1,74 @@
+# example 8.3: bayesian multilevel regression with random slopes and a partially factored (multivariate) distribution for incomplete predictors and latent response scores
+
+library(rblimp)
+
+data_url <- "https://raw.githubusercontent.com/craigenders/amd-book-examples/main/Data/diary.rda"
+load(gzcon(url(data_url, open = "rb")))
+
+# default prior2 (wishart prior for level-2 covariance matrix with  ss = 0 and df = -v - 1)
+analysis1 <- rblimp(
+    data = diary,
+    clusterid = 'person',
+    fixed = 'female',
+    center = '
+      groupmean = pain;  # center at latent group means
+      grandmean = pain.mean sleep painaccept',  # center at grand means
+    model = 'posaff ~ pain@b1 sleep pain.mean@b3 painaccept female | pain', # label within- and between-cluster slope coefficients 
+    waldtest = 'b1 = b3', # test whether within- and between-cluster regressions differ
+    seed = 90291,
+    burn = 5000,
+    iter = 10000)
+
+output(analysis1)
+
+# prior1 (wishart prior for level-2 covariance matrix with  ss = 0 and df = v + 1)
+analysis2 <- rblimp(
+  data = diary,
+  clusterid = 'person',
+  fixed = 'female',
+  center = '
+      groupmean = pain;  # center at latent group means
+      grandmean = pain.mean sleep painaccept',  # center at grand means
+  model = 'posaff ~ pain@b1 sleep pain.mean@b3 painaccept female | pain', # label within- and between-cluster slope coefficients 
+  waldtest = 'b1 = b3', # test whether within- and between-cluster regressions differ
+  seed = 90291,
+  burn = 5000,
+  iter = 10000,
+  options = 'prior1')
+
+output(analysis2)
+
+# prior3 (wishart prior for level-2 covariance matrix with  ss = 0 and df = v + 1)
+analysis3 <- rblimp(
+  data = diary,
+  clusterid = 'person',
+  fixed = 'female',
+  center = '
+      groupmean = pain;  # center at latent group means
+      grandmean = pain.mean sleep painaccept',  # center at grand means
+  model = 'posaff ~ pain@b1 sleep pain.mean@b3 painaccept female | pain', # label within- and between-cluster slope coefficients 
+  waldtest = 'b1 = b3', # test whether within- and between-cluster regressions differ
+  seed = 90291,
+  burn = 5000,
+  iter = 10000,
+  options = 'prior3')
+
+output(analysis3)
+
+# separation prior
+analysis4 <- rblimp(
+  data = diary,
+  clusterid = 'person',
+  latent = 'person = painslp',  # define random slope as a separate level-2 latent variable
+  fixed = 'female',
+  center = '
+      groupmean = pain;  # center at latent group means
+      grandmean = pain.mean sleep painaccept',  # center at grand means
+  model = 'posaff ~ pain@b1 sleep pain.mean@b3 painaccept female pain*painslp@1', # label within- and between-cluster slope coefficients painslp ~ 1@0, # fix mean of random slope latent variable to 0 posaff[person] ~~ painslp', # correlation between random intercepts and random slope latent variable 
+  waldtest = 'b1 = b3', # test whether within- and between-cluster regressions differ
+  seed = 90291,
+  burn = 5000,
+  iter = 10000,
+  options = 'use_phantom')
+
+output(analysis4)
---
+++ rblimp/8.3 - Model-Based Multiple Imputation.R
@@ -0,0 +1,46 @@
+# example 8.3: bayesian multilevel regression with random slopes and a partially factored (multivariate) distribution for incomplete predictors and latent response scores
+
+library(rblimp)
+
+data_url <- "https://raw.githubusercontent.com/craigenders/amd-book-examples/main/Data/diary.rda"
+load(gzcon(url(data_url, open = "rb")))
+
+analysis <- rblimp(
+  data = diary,
+  clusterid = 'person',
+  fixed = 'female',
+  center = '
+      groupmean = pain;  # center at latent group means
+      grandmean = pain.mean sleep painaccept',  # center at grand means
+  model = 'posaff ~ pain@b1 sleep pain.mean@b3 painaccept female | pain', # label within- and between-cluster slope coefficients 
+  waldtest = 'b1 = b3', # test whether within- and between-cluster regressions differ
+  seed = 90291,
+  burn = 5000,
+  iter = 10000, 
+  nimps = 100,
+  chains = 100)
+
+output(analysis)
+
+# mitml list
+implist <- as.mitml(analysis)
+
+# pooled grand means
+mean_sleep <- mean(unlist(lapply(implist, function(data) mean(data$sleep))))
+mean_pain.l2mean <- mean(unlist(lapply(implist, function(data) mean(data$pain.mean.person.))))
+mean_painaccept <- mean(unlist(lapply(implist, function(data) mean(data$painaccept))))
+
+# center at latent cluster means
+for (i in 1:length(implist)) {
+  implist[[i]]$pain.cwc <- implist[[i]]$pain - implist[[i]]$pain.mean.person.
+}
+
+# analysis and pooling
+model <- "posaff ~ pain.cwc + I(sleep - mean_sleep) + I(pain.mean.person. - mean_pain.l2mean) + I(painaccept - mean_painaccept) + female + (1 + pain.cwc | person)"
+pooled <- with(implist, lmer(model, REML = T))
+
+# significance tests with barnard & rubin degrees of freedom
+df <- 132 - 5 - 1
+estimates <- mitml::testEstimates(pooled, extra.pars = T, df.com = df)
+estimates
+confint(estimates, level = .95)
