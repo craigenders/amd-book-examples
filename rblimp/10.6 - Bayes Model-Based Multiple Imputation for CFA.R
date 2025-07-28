7c1e6c25ef4bddd6f1d14f833a85d3f0bc1b7612
craigenders <cenders@ucla.edu>
2025-07-28 09:05:16 -0700

    Updates

---
+++ rblimp/10.6 - Bayes Model-Based Multiple Imputation for CFA.R
@@ -0,0 +1,57 @@
+# example 10.6 - bayes cfa-based imputation for ordered categorical indicators,
+
+library(lavaan.mi)
+library(rblimp)
+
+data_url <- "https://raw.githubusercontent.com/craigenders/amd-book-examples/main/Data/eatingrisk.rda"
+load(gzcon(url(data_url, open = "rb")))
+
+impute <- rblimp(
+    data = eatingrisk,
+    ordinal = 'drive1:drive7 diet1:diet5',
+    latent = 'drive diet',
+    model = '
+      factor.model: 
+      drive ~~ diet;
+      drive -> drive1:drive7;
+      diet -> diet1:diet5;
+      auxiliary.variable.model:
+      bmi ~ drive1:drive7 diet1:diet5', 
+    seed = 90291,
+    burn = 30000,
+    iter = 10000,
+    nimps = 100,
+    chains = 100)
+
+output(impute)
+
+# mitml list
+implist <- as.mitml(impute)
+
+# cfa model
+lavaan_model <- '
+  drive ~~ diet
+  drive =~ 1*drive1 + drive2 + drive3 + drive4 + drive5 + drive6 + drive7
+  diet =~ 1*diet1 + diet2 + diet3 + diet4 + diet5
+'
+
+# cfa model w latent response variable indicators
+lavaan_model_latent <- '
+  drive ~~ diet
+  drive =~ 1*drive1.latent + drive2.latent + drive3.latent + drive4.latent + drive5.latent + drive6.latent + drive7.latent
+  diet =~ 1*diet1.latent + diet2.latent + diet3.latent + diet4.latent + diet5.latent
+'
+
+# fit model with ml and latent response scores
+pooled_ml <- cfa.mi(lavaan_model_latent, data = implist, estimator = "ml")
+summary(pooled_ml, standardized = T, fit = T)
+
+# imputation-based modification indices
+modindices.mi(pooled_ml, op = c("~~","=~"), minimum.value = 3, sort. = T)
+
+# fit model with wls
+pooled_wls <- cfa.mi(lavaan_model, data = implist, std.lv = T, estimator = "wls")
+summary(pooled_wls, standardized = T)
+
+# imputation-based modification indices
+modindices.mi(pooled_wls, op = c("~~","=~"), minimum.value = 3, sort. = T)
